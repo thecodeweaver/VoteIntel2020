4038b14846f0395b874721c3e1dd8a0a60128b67
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-08-01 19:01:01 -0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vote Intel 2020</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <style>
+        .jumbotron {
+                background-color: #4b4a4d;
+                color: #fff;
+                padding: 70px 25px;
+        }
+    </style>
+</head>
+
+<?php
+    # Some PHP to get Heroku to host this lol
+    echo "VoteIntel2020: Made With <3 by TheCodeWeaver";
+?>
+
+<body>
+    <div class="jumbotron text-center">
+        <h1>Vote Intel 2020</h1>
+        <p>It's up to you to bring about change during the 2020 election!</p>
+        <iframe src="https://free.timeanddate.com/countdown/i7ebuijw/n137/cf111/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pc00f/tcf00/fs100/szw320/szh135/tatElection%20Day%202020%20Countdown/tac000/tptElection%20Day%202020%20Is%20Behind/tpc000/mac000/mptHopefully%20you%20have%20brought%20about%20change/mpc000/iso2020-11-03T00:00:00" allowTransparency="true" frameborder="0" width="320" height="135"></iframe>
+      </div>
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <h3>Why Should You Vote?</h3>
+            <p>Voting Will Help Solve Many Issues Such As...</p>
+            <ul class="list-group">
+                <li class="list-group-item">Terrible Response To COVID-19 Pandemic</li>
+                <li class="list-group-item">Racial Injustice and Police Brutality</li>
+                <li class="list-group-item">Global Warming and Other Environmental Issues</li>
+                <li class="list-group-item">And Much More!</li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <h3><a href="https://projects.fivethirtyeight.com/polls/">General Election Poll Results</a></h3>
+          </div>
+            <div class="col-sm-4">
+                <h3>Voting Resources</h3>
+                <p>Currently TODO</p>
+            </div>
+        </div>
+      </div>
+
+    <footer class="container-fluid text-center">
+        </a>
+        <p>Made With Love By <a href="https://thecodeweaver.github.io/" title="TheCodeWeaver's Website">TheCodeWeaver</a></p>
+    </footer>
+</body>
+</html>
